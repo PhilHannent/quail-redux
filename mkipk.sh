2e5d6c31c08ed3618a10f68f557c8c88abef4cb8
chipx86 <none@none>
2003-07-09 23:43:25 +0000

    Fixed up the ipk building script.

--- mkipk.sh
+++ mkipk.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 # mkipk.sh
-# generates an ipkg for Tickypip
+# generates an ipkg for qpe-gaim
 
 # Based off the mkipk.sh by Bill Kendrick <bill@newbreedsoftware.com>
 
 # Revised by Christian Hammond <chipx86@gnupdate.org>
+
+if [ ! -e $HOME/bin/dev-arm-qpe.sh ]; then
+	echo "$HOME/bin/dev-arm-qpe.sh is not present."
+	echo "This is required for setting the ARM development environment "
+	echo "variables."
+	exit 1
+fi
 
 . ./mkstuff.conf
 
@@ -18,19 +25,22 @@ mkdir -p $TMPDIR/BUILD/DIST/CONTROL
 
 echo
 echo "COPYING SOURCES INTO BUILD AREA"
-cp -av src images levels tickypip.pro $TMPDIR/BUILD/
+cp -av src data gaim.pro $TMPDIR/BUILD/
 
-# Modify the tickypip.pro
-perl -pi -e "s/debug/release/g" $TMPDIR/BUILD/tickypip.pro
-perl -pi -e "s/LOCAL_COMPILE//g" $TMPDIR/BUILD/tickypip.pro
+# Modify the gaim.pro
+perl -pi -e "s/debug/release/g" $TMPDIR/BUILD/gaim.pro
+perl -pi -e "s/LOCAL_COMPILE//g" $TMPDIR/BUILD/gaim.pro
 
 # Remove the xcf files
-$RM $TMPDIR/BUILD/images/*.xcf
-$RM -rf $TMPDIR/BUILD/images/.xvpics
-$RM -rf $TMPDIR/BUILD/images/CVS
-$RM -rf $TMPDIR/BUILD/levels/CVS
-$RM -rf $TMPDIR/BUILD/levels/FORMAT*
-$RM -rf $TMPDIR/BUILD/levels/*.pdb
+$RM -f $TMPDIR/BUILD/images/*.xcf
+$RM -rf $TMPDIR/BUILD/data/images/.xvpics
+$RM -rf $TMPDIR/BUILD/data/images/CVS
+$RM -rf $TMPDIR/BUILD/data/images/protocols/.xvpics
+$RM -rf $TMPDIR/BUILD/data/images/protocols/CVS
+$RM -rf $TMPDIR/BUILD/data/images/protocols/small/.xvpics
+$RM -rf $TMPDIR/BUILD/data/images/protocols/small/CVS
+$RM -rf $TMPDIR/BUILD/data/images/status/.xvpics
+$RM -rf $TMPDIR/BUILD/data/images/status/CVS
 $RM -rf $TMPDIR/BUILD/src/CVS
 $RM -rf $TMPDIR/BUILD/CVS
 cd $TMPDIR/BUILD
@@ -38,7 +48,7 @@ cd $TMPDIR/BUILD
 echo
 echo "MAKING SURE BINARY EXISTS"
 . $HOME/bin/dev-arm-qpe.sh
-tmake tickypip.pro -o Makefile
+tmake gaim.pro -o Makefile
 make clean
 make
 
@@ -56,44 +66,42 @@ echo "CREATING CONTROL FILE"
 echo "Package: $PACKAGE" > $CONTROL
 echo "Priority: optional" >> $CONTROL
 echo "Version: $VER" >> $CONTROL
-echo "Section: games" >> $CONTROL
+echo "Section: qpe/applications" >> $CONTROL
 echo "Architecture: $ARCH" >> $CONTROL
 echo "Maintainer: Christian Hammond <chipx86@gnupdate.org>" >> $CONTROL
-echo "Description: Connect the computers together to form an active network." >> $CONTROL
+echo "Description: A multi-protocol instant messenger client, supporting AIM, ICQ, MSN, Yahoo!, Jabber, IRC, Zephyr, Gadu-Gadu, Napster, and Trepia." >> $CONTROL
 
 echo
 echo "COPYING DATA FILES"
 
-mkdir -p $DIST/opt/QtPalmtop/share/tickypip
-mkdir $DIST/opt/QtPalmtop/share/tickypip/images/
-cp -R $TMPDIR/BUILD/images/* $DIST/opt/QtPalmtop/share/tickypip/images/
-mkdir -p $DIST/opt/QtPalmtop/share/tickypip/levels/
-cp -R $TMPDIR/BUILD/levels/* $DIST/opt/QtPalmtop/share/tickypip/levels/
+mkdir -p $DIST/opt/QtPalmtop/share/gaim
+mkdir $DIST/opt/QtPalmtop/share/gaim/data
+cp -R $TMPDIR/BUILD/data/* $DIST/opt/QtPalmtop/share/gaim/data/
 
 echo
 echo "CREATING BINARIES"
 
 mkdir -p $DIST/opt/QtPalmtop/bin/
-cp $TMPDIR/BUILD/tickypip $DIST/opt/QtPalmtop/bin/
+cp $TMPDIR/BUILD/gaim $DIST/opt/QtPalmtop/bin/
 
 echo
 echo "STRIPPING THE BINARY"
-arm-linux-strip $DIST/opt/QtPalmtop/bin/tickypip
+arm-linux-strip $DIST/opt/QtPalmtop/bin/gaim
 
 echo
 echo "CREATING ICON AND DESKTOP FILE"
 
 mkdir -p $DIST/opt/QtPalmtop/pics/
-cp tickypip.png $DIST/opt/QtPalmtop/pics/tickypip.png
+cp gaim.png $DIST/opt/QtPalmtop/pics/gaim.png
 
 mkdir -p $DIST/opt/QtPalmtop/apps/Games/
-DESKTOP=$DIST/opt/QtPalmtop/apps/Games/tickypip.desktop
+DESKTOP=$DIST/opt/QtPalmtop/apps/Games/gaim.desktop
 echo "[Desktop Entry]" > $DESKTOP
-echo "Comment=Connect the computers together to form an active network." >> $DESKTOP
-echo "Exec=tickypip" >> $DESKTOP
-echo "Icon=tickypip" >> $DESKTOP
+echo "Comment=A multi-protocol instant messenger client." >> $DESKTOP
+echo "Exec=gaim" >> $DESKTOP
+echo "Icon=gaim" >> $DESKTOP
 echo "Type=Application" >> $DESKTOP
-echo "Name=Tickypip" >> $DESKTOP
+echo "Name=Gaim" >> $DESKTOP
 
 
 echo
