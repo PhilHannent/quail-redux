bcba85eee0fff2b4910f23aef96e17deb5db1c57
Phil Hannent <phil@hannent.co.uk>
2013-09-27 14:41:12 +0100

    Tidy misc files, part 2

--- install/make-msi.bat
+++
@@ -1,2 +0,0 @@
-cmd /c "c:\Program Files (x86)\WiX Toolset v3.7\bin\candle.exe" WindowsInstallScript.wxs
-cmd /c "c:\Program Files (x86)\WiX Toolset v3.7\bin\light.exe" -ext WixUiExtension WindowsInstallScript.wixobj -out Quail-installer.msi
