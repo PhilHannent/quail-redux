b2fd98e358293f7cfebf930d4cf89d26960785ee
Phil Hannent <phil@hannent.co.uk>
2014-02-17 14:59:37 +0000

    Centralise the version infromation into one file

--- version.h
+++ version.h
@@ -1,12 +1,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
+#define APP_DISPLAY_VERSION         "0.1.0"
+#define APP_MAJOR_VERSION           "0"
+#define VER_FILEVERSION             0,1,0,0
+
 #define VER_FILEVERSION_STR         "0.1.0.0\0"
 
 #define VER_COMPANYNAME_STR         "Phil Hannent"
 #define VER_FILEDESCRIPTION_STR     "Instant Messaging Software"
 #define VER_INTERNALNAME_STR        "Quail"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2013-2014 Phil Hannent"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright \u00A9 2013-2014 Phil Hannent"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "Quail.exe"
